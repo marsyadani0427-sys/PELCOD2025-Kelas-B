09f790e71cdcc8542583ee2b2b8eee34f6ff33a0
[200~marsyadani0427-sys~ <m.arsyadani0427@gmail.com>
2025-09-13 11:42:29 +0700

    250441100054_dani

---
+++ 250441100054_dani/dani.py
@@ -0,0 +1,33 @@
+# Nama = "M.ARSYADANI"
+# NIM = 250441100054
+# Semester = 1
+
+# print("=====BIODATA MAHASISWA=====")
+# print("Hallo Nama saya:" + Nama + "\n")
+# print(f"NIM saya: {NIM}\n")
+# print("Saya berkuliah di semester : {}".format(Semester))
+
+# a = 10
+# b = 5
+# c = a + b
+# d = a - b
+# e = a * b
+# f = a / b
+# g = a % b
+# print("Hasil penjumlahan :", c)
+# print("Hasil pengurangan :", d)
+# print("Hasil perkalian :", e)
+# print("Hasil pembagian :", f)
+# print("Hasil sissa bagi :", g)
+
+nama = input("Nama mahasiswa : ")
+umur = input("Umur : ")
+fakultas = input("Fakultas : ")
+prodi = input("Prodi : ")
+buah = input("Buah favorit : ")
+
+print("Nama " + nama)
+print("Umur " + umur + "tahun")
+print("Fakultas " + fakultas)
+print("Prodi " + prodi)
+print("Buah favorit " + buah )
